2792e87d99f810326c1e46f40fe15df7aee86c9f
Mateonidas <teus.polec@gmail.com>
2020-07-17 13:19:08 +0200

    Initial commit

---
+++ src/main/java/com/project/rpg/projectrpg/ProjectRpgApplication.java
@@ -0,0 +1,13 @@
+package com.project.rpg.projectrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/rpg/projectrpg/ProjectRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rpg.projectrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
