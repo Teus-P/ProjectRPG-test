b9c6e0ac1bf1c137c5062dab3dac4f7a8d01990b
Mateonidas <teus.polec@gmail.com>
2020-09-11 19:42:15 +0200

    Added configuration of internationalization in application.

--- src/main/java/com/project/rpg/projectrpg/configuration/Properties.java
+++ src/main/java/com/project/rpg/projectrpg/configuration/Properties.java
@@ -1,4 +1,4 @@
-package com.project.rpg.projectrpg.properties;
+package com.project.rpg.projectrpg.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
